f47503c72f736e942dbe75d77c41229d560599e3
Muhammad Naeem <naeem863@Muhammads-MacBook-Pro.local>
2025-04-18 12:29:44 +0500

    routing issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,35 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./container/Page";
 import Login from "./auth/Login";
 import Dashboard from "./admin/container/Dashboard";
 import ProtectedRoutes from "./routes/ProtectedRoutes";
 import Add_blog_Page from "./admin/pages/Add_blog_Page";
+
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" index element={<Page />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoutes>
-                <Dashboard />
-              </ProtectedRoutes>
-            }
-          />
-          <Route
-            path="/blogs/add-new"
-            element={
-              <ProtectedRoutes>
-                <Add_blog_Page />
-              </ProtectedRoutes>
-            }
-          ></Route>
-        </Routes>
-      </Router>
-    </>
+    <BrowserRouter basename="/DecimaSol-Landing-Page">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Page />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoutes>
+              <Dashboard />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/blogs/add-new"
+          element={
+            <ProtectedRoutes>
+              <Add_blog_Page />
+            </ProtectedRoutes>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
